965665062ecaba341e2883271412ceb97b723b7e
Abdus Sattar <sasorganicagro@gmail.com>
2023-11-15 18:19:51 +0600

    Set up next.js project using typeScript

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const ContactPage = () => {
+  return (
+    <div>
+      Contact page
+      <Link href='/'>Go to Home</Link>
+      </div>
+  )
+}
+
+export default ContactPage;
