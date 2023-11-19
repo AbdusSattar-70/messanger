f57fdfc7e64bf4c79a922c5bed1636e3c7e492ca
Abdus Sattar <sasorganicagro@gmail.com>
2023-11-19 22:27:12 +0600

    Add curry function

--- zLearnTs.js
+++ zLearnTs.js
@@ -1,10 +1,11 @@
-var showValue = function(obj) {
-  var values = Object.values(obj);
-  if (values.length > 0) {
-    return values.join(',');
+const curryFn = (fnArr) => {
+  return (x) => {
+    let res = x;
+    for(let i = fnArr.length - 1; i >= 0; i--){
+      res = fnArr[i](res);
+    }
+    return res;
   }
-  return "empty";
 };
-
-var result = showValue({ name: "sattar", age: 23 });
-console.log(result);
+const newCurry = curryFn([x => x + 1, x => x * x, x => 2 * x]);
+console.log(newCurry(4));
